de2ed16653ff2c62c62f30d1694228ae955c4be8
Rasel <rasel440@gmail.com>
2022-07-05 00:42:38 +0600

    edited

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 
 class homeController extends Controller{
     public function index(){
-        $my_name="Rasel";
-        $date=[
-            'name'=>$my_name
-        ];
-        return view('home.index',$date);
+   
+        $this->data['name']="Rasel";
+        $this->data['add']="Mirpur,Dhaka";
+        
+        return view('home.index',$this->data);
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,7 @@
 <html>
     <title>This is Index Page</title>
     <body>
-        <?php echo $name;?>
+        <?php echo $name, $add;?>
         <h1> Welcome from Index page view</h1>
     </body>
 </html>
